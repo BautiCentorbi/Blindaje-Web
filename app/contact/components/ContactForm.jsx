58d0e6a1084cb6e18adf8800a6ea1e33801a8093
Bautista Centorbi <bautii.centorbii@gmail.com>
2025-07-02 13:34:30 -0300

    Changes in Resend 1.2

--- app/contact/components/ContactForm.jsx
+++ app/contact/components/ContactForm.jsx
@@ -85,7 +85,7 @@ const ContactForm = () => {
         <div className="flex flex-col gap-2 mb-4 md:mb-2 2xl:mb-4">
           <label className="text-sm font-semibold">Asunto</label>
           <input
-            name="mensaje"
+            name="asunto"
             placeholder="PUESTO: ..."
             required
             className="h-10 p-4 rounded-lg bg-gray-100"
