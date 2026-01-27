5556602162ee53dcddebe1460e90683cbb2afaf9
Bautista Centorbi <bautii.centorbii@gmail.com>
2026-01-27 12:11:35 -0300

    BLINDAJE Web - Email in Form Fields Add 1.1

--- app/cotizaciones/components/BudgetForm.jsx
+++ app/cotizaciones/components/BudgetForm.jsx
@@ -91,6 +91,17 @@ const BudgetForm = () => {
         </div>
 
         <div className="flex flex-col gap-2 mb-4 md:mb-2 2xl:mb-4">
+          <label className="text-sm font-semibold">Email</label>
+          <input
+            name="email"
+            className="h-10 p-4 rounded-lg bg-gray-100"
+            type="email"
+            placeholder="rV2bD@example.com"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-2 mb-4 md:mb-2 2xl:mb-4">
           <label className="text-sm font-semibold">Asunto</label>
           <input
             name="asunto"
